5711d36fed4a64efd2441ab9594cf6c0de3c6b28
Pedro Mendiola <pedro.enrrique29@outlook.com>
2021-04-21 23:54:24 -0300

    Cadastros Estudantes Part 1 Concluida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EstudanteComponent } from './estudante/estudante.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { EstudanteComponent } from './estudante/estudante.component';
     EstudanteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
